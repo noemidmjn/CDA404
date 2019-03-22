a5e01de4781f9c3f263bd2fa25a77c8909355705
Noemi Domjan <4domjn96@solent.ac.uk>
2019-03-22 23:53:13 +0000

    sg

---
+++ s8/task_1/main.js
@@ -0,0 +1,13 @@
+function main() {
+    alert('working');
+    var randomNumber = Math.round(Math.random() * (20 - 10) + 10);
+
+}
+
+function buttonClicked () {
+    var guessedNumber = document.getElementById("num_guessed");
+    if guessedNumber === randomNumber {
+
+    }
+}
+
